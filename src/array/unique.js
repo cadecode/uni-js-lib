608fe218dfaee157f2b5028d292545665c76e5d6
Cade Li <cadecode@foxmail.com>
2021-06-17 20:19:23 +0800

    feat: 添加并测试数组去重方法-unique/uniqueBySet

---
+++ src/array/unique.js
@@ -0,0 +1,12 @@
+const unique = (arr) => {
+    return arr.filter((item, index) => arr.indexOf(item) === index);
+};
+
+const uniqueBySet = (arr) => {
+    return [...new Set(arr)];
+};
+
+export default {
+    unique,
+    uniqueBySet
+}
